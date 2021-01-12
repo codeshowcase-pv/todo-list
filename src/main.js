065827038ffe846ecaf8b8304740ff6f8df8a493
rasskazov-pv <rasskazov.paul@yandex.ru>
2021-01-12 22:23:00 +0300

    Add Vuex, create store for App

--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
 // импорт метода createApp из модуля vue
 import { createApp } from 'vue'
+// импорт метода для создания vuex
+import { createStore } from "vuex";
+// импорт компонента App
+import App from './App.vue'
+
+// создание приложения с корневым компонентом App
+const app = createApp(App);
+// создание хранилища vuex
+const store = createStore({
+    state () {
+        return {
+            todos: []
+        }
+    },
+    mutations: {
+        add_new_todo(state, todo) {
+            state.todos.push(todo);
+        },
+        delete_todo(state, key) {
+            state.todos.splice(key, 1);
+        }
+    }
+});
 
 // подключение css библиотеки бутстрап, js и css
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// импорт компонента App
-import App from './App.vue'
 
-// вызов метода создания приложения, в параметр передаётся корневой компонент
+
+// подключение модуля vuex
+app.use(store);
 // приложение добавляется в элемент с id=app в index.html
-// использовать модуль VueMaterial в приложении
-createApp(App).mount('#app');
+app.mount('#app');
+
